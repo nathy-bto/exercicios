737a94fa25ced8815750908d6b28d3cd5205a84d
Nathy Brito <129338818+nathy-bto@users.noreply.github.com>
2023-06-28 20:34:56 +0000

    atvd 1 recursividade

---
+++ recursiv.c
@@ -0,0 +1,25 @@
+//Escreva uma função recursiva que calcule a soma dos primeiros n cubos:
+#include <stdio.h>
+int main() {
+    int valor = 0; 
+    int resultado;
+    int n;
+
+    int cubo(int n)
+    {
+     // aqui temos uma função. temos que declarar o tipo da função antes dela. A função recebe como parâmetro a variável n.
+    //O tipo de retorno da função é int, ou seja, ela retorna um valor inteiro. 
+    if (n == 0)
+        return n;
+    else
+        return (n*n*n) + cubo(n-1);
+    }
+
+    printf("digite um valor para ser calculado a soma dos cubos: ");
+    scanf("%d", &valor);
+    resultado = cubo(n);
+
+    printf("o resultado da soma dos primeiros %d cubos é: %d", valor, resultado);
+
+return 0;
+}
