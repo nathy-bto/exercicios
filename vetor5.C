09e8ab9138ca2187d784cd5e4b1a8bc20a5a3b12
Nathy Brito <129338818+nathy-bto@users.noreply.github.com>
2023-07-05 23:16:23 +0000

    vetor

---
+++ vetor5.C
@@ -0,0 +1,23 @@
+//Faça um programa que leia um vetor de oito posições. Em seguida, leia também dois valores X e Y quaisquer correspondentes a duas posições no vetor.
+//Seu programa deverá exibir a soma dos valores encontrados nas respectivas posições X e Y.
+#include <stdio.h>
+int main (){
+    int vet[8];
+    int valores;
+    for (int i = 0; i < 7; i++)
+    {
+        printf("digite os valores: ");
+        scanf("%d", &vet[i]);
+    }
+    printf("dos valores mostrados, escolha um: ");
+    for (int i = 0; i < 7; i++);
+    {
+        
+    }
+    scanf("%d", &valores);
+    soma 
+
+    
+
+
+}
